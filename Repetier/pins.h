f6e9d129cf9252ba22cb0f84f798aac74924e0a6
David Govea <govea.d@gmail.com>
2013-07-15 20:04:41 -0700

    pin typo

--- Repetier/pins.h
+++ Repetier/pins.h
@@ -693,6 +693,7 @@ STEPPER_CURRENT_CONTROL
 
       #define Z_MIN_PIN          -1
       #define Z_MAX_PIN          20
+    #endif
 
     #define TEMP_0_PIN          7   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!! (pin 33 extruder)
     #define TEMP_1_PIN          6   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!! (pin 34 bed)
